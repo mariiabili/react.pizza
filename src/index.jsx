c55a8721c312ac23dc87889f8b1d24be3de1f83c
Bilion <bilionjenia1111@gmail.com>
2025-04-17 19:00:32 +0200

    added code structure

---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const rootElem = document.getElementById('root');
+
+if (rootElem) {
+	const root = ReactDOM.createRoot(rootElem);
+
+	root.render(<App />);
+}
